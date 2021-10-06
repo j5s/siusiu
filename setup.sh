5ab9d609902bf913352d0070e3072f2b5984bfe1
无在无不在 <2227627947@qq.com>
2021-10-06 16:39:19 +0800

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,18 +4,16 @@ setup_path="$HOME/src"
 bin_path=$HOME/bin
 app_name=siusiu
 function setup {
+    git config --global http.sslverify false
     git clone https://gitee.com/nothing-is-nothing/siusiu.git $1/$app_name
     cd $1/$app_name
     go build -o $app_name
     mv $app_name $2/
-    #删除含有app_name的所有行
-    sed -i "/$app_name/d" $HOME/.zshrc
-    sed -i "/$app_name/d" $HOME/.bash_profile
-    #向.zshrc和.bash_profile 中添加别名
-    echo "alias $app_name=$2/$app_name" >> $HOME/.zshrc
-    echo "alias $app_name=$2/$app_name" >> $HOME/.bash_profile
-    source $HOME/.zshrc
-    source $HOME/.bash_profile
+    for shell_config_file in "$HOME/.zshrc" "$HOME/.bash_profile" "$HOME/.bashrc"; do
+        sed -i "/$app_name/d" $shell_config_file                 #删除含有app_name的所有行
+        echo "alias $app_name=$2/$app_name" >>$shell_config_file #向shell配置文件中添加别名
+        source $shell_config_file
+    done
     echo "[*] setup success!"
 }
 
