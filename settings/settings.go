88371720ea938720fad69367d311b839cbc32e65
无在无不在 <2227627947@qq.com>
2021-10-06 15:46:34 +0800

    fix /Users/mac

--- settings/settings.go
+++ settings/settings.go
@@ -1,13 +1,23 @@
 package settings
 
+import (
+	"fmt"
+	"os"
+)
+
 //Config 配置
 type Config struct {
 	ShellPrompt  string
 	MyVendorPath string
 }
 
-//AppConfig App配置
-var AppConfig Config = Config{
-	ShellPrompt:  "siusiu >",
-	MyVendorPath: "$HOME/src/siusiu/myvendor",
+func newAppConfig() *Config {
+	home := os.Getenv("HOME")
+	return &Config{
+		ShellPrompt:  "siusiu > ",
+		MyVendorPath: fmt.Sprintf("%s/src/siusiu/myvendor", home),
+	}
 }
+
+//AppConfig App配置
+var AppConfig *Config = newAppConfig()
