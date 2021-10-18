a60bd3becd9f8310e168060998a35ee16935e700
无在无不在 <2227627947@qq.com>
2021-10-18 21:51:22 +0800

    update

--- myvendor/url-collector/run.sh
+++ myvendor/url-collector/run.sh
@@ -5,7 +5,7 @@ install_path=$base_path/url-collector #程序安装目录
 function download {
     git clone https://gitee.com/nothing-is-nothing/url-collector.git $1
     cd $1
-    go build
+    go build ./... && go build
 }
 
 #1.检查程序目录是否存在
@@ -15,4 +15,5 @@ if [ ! -d $install_path ]; then
     download $install_path
     echo "[*] download success"
 fi
+
 $install_path/url-collector $*
