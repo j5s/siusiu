680fcb227b0758b41a1c6d7715343bad15468884
无在无不在 <2227627947@qq.com>
2021-10-19 12:51:52 +0800

    fix

--- myvendor/url-collector/run.sh
+++ myvendor/url-collector/run.sh
@@ -13,5 +13,5 @@ if [ ! -d $install_path ]; then
     download $install_path
     echo "[*] download success"
 fi
-cd $install_path && git pull origin master 
+cd $install_path && git reset --hard && git pull origin master 
 $install_path/url-collector $*
